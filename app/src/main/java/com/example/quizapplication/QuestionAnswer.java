83a66b9c2f6206cdec7336a6c19200cc66959d10
satriaww <76892220+Babybay@users.noreply.github.com>
2024-01-22 19:53:07 +0700

    first commit

---
+++ app/src/main/java/com/example/quizapplication/QuestionAnswer.java
@@ -0,0 +1,30 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Apa ibukota Indonesia?",
+            "Siapa tokoh fiksi yang terkenal sebagai manusia laba-laba?",
+            "Siapa penulis novel 'Harry Potter?'",
+            "Berapakah jumlah warna pelangi?",
+            "Apa kepanjangan dari BINUS?"
+    };
+
+    public static String choices[][] = {
+            {"Jakarta","Jekardah","Bogor","Venus"},
+            {"Spooder-Man","Spider-Man","Harry Maguire","Tobby Maguire"},
+            {"J.K Rowling","Nikola Tesla","Naruto","Voldemort"},
+            {"1","2","5","7"},
+            {"Bina nus", "Binussus", "Bina Nusantara", "University"}
+    };
+
+    public static String correctAnswers[] = {
+            "Jakarta",
+            "Spider-Man",
+            "J.K Rowling",
+            "7",
+            "Bina Nusantara"
+
+    };
+
+}
